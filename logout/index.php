7a0cd72ae38162c859dd54054754c06ab63d4018
Sithu <cithukyaw@gmail.com>
2021-04-18 16:18:58 +0630

    User token instead of timestamp upon logout

--- logout/index.php
+++ logout/index.php
@@ -1,15 +1,17 @@
 <?php
+
 if (auth_isAnonymous()) {
     flash_set(_t('Your session is expired.'), '', 'error');
     _redirect(_cfg('baseDir') . '/login');
 }
 
-$timestamp = _arg(2);
-if ($timestamp) {
-    if ($timestamp == $_auth->timestamp) {
-        # Normal logout process
-        auth_clear();
-        flash_set(_t('You have signed out successfully.'));
-        _redirect(_cfg('baseDir') . '/login');
-    }
+$token = _arg(2);
+if ($token && $token == _app('auth')->token) {
+    # Normal logout process
+    auth_clear();
+    flash_set(_t('You have signed out successfully.'));
+
+    _redirect(_cfg('baseDir') . '/login');
 }
+
+_page404();
