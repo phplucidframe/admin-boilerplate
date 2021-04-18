0cec4bc3e4fae052c93a830dd8b37bfdf1b924cc
Sithu <cithukyaw@gmail.com>
2021-04-18 16:16:39 +0630

    Disable layoutMode and

--- inc/site.config.php
+++ inc/site.config.php
@@ -1,7 +1,10 @@
 <?php
-##############################################
-# This is a site-specific configuration file #
-##############################################
+/**
+ * This is a site-specific configuration file
+ * You can override the configuration variables in /inc/config.php or /app/inc/site.config.php from here and
+ * you can also create additional configuration for your site.
+ */
+
 $lc_baseDir = 'admin';
 # Translation disabled for the Admin panel
 $lc_translationEnabled = false;
@@ -9,5 +12,5 @@ $lc_translationEnabled = false;
 $lc_cleanURL = true;
 # $lc_itemsPerPage: number of items per page in pager
 $lc_itemsPerPage = 20;
-# $lc_reqSign: form input required sign
-$lc_reqSign = '<span class="required">*</span>';
+# $lc_layoutMode: Enable layout mode or not
+$lc_layoutMode = false;
